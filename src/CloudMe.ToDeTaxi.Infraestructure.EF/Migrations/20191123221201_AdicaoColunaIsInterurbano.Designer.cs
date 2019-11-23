e4a1706ecf6f7b65565753b050e2658827bf005a
José Rodolfo Campos Rios <jrodolforios@hotmail.com>
2019-11-23 19:12:42 -0300

    Adição de migration

--- src/CloudMe.ToDeTaxi.Infraestructure.EF/Migrations/20191123221201_AdicaoColunaIsInterurbano.Designer.cs
+++ src/CloudMe.ToDeTaxi.Infraestructure.EF/Migrations/20191123221201_AdicaoColunaIsInterurbano.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using CloudMe.ToDeTaxi.Infraestructure.EF.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace CloudMe.ToDeTaxi.Infraestructure.EF.Migrations
 {
     [DbContext(typeof(CloudMeToDeTaxiContext))]
-    partial class CloudMeToDeTaxiContextModelSnapshot : ModelSnapshot
+    [Migration("20191123221201_AdicaoColunaIsInterurbano")]
+    partial class AdicaoColunaIsInterurbano
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
