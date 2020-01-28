3201d221bf108aecaec049d32a3460c218ea824c
Rodolfo Rios <jrodolforios@hotmail.com>
2020-01-28 19:12:33 -0300

    AJuste na checagem de email do usuário

--- src/CloudMe.MotoTEX.Domain.Services/UsuarioService.cs
+++ src/CloudMe.MotoTEX.Domain.Services/UsuarioService.cs
@@ -152,7 +152,7 @@ namespace CloudMe.MotoTEX.Domain.Services
                 AddNotification("Usuários", string.Format("CPF '{0}' está sendo utilizado por outro usuário", summary.CPF));
             }
 
-            var usrEmail = _userRepository.Search(usr => usr.Email == summary.Email).FirstOrDefault();
+            var usrEmail = await _userManager.FindByEmailAsync(summary.Email);
             if (usrEmail != null && !string.IsNullOrEmpty(summary.Email))
             {
                 AddNotification("Usuários", string.Format("Email '{0}' está sendo utilizado por outro usuário", summary.Email));
@@ -297,6 +297,12 @@ namespace CloudMe.MotoTEX.Domain.Services
                 if (usrCPF != null && !string.IsNullOrEmpty(summary.CPF))
                 {
                     AddNotification("Usuários", string.Format("CPF '{0}' está sendo utilizado por outro usuário", summary.CPF));
+                }
+
+                var usrEmail = await _userManager.FindByEmailAsync(summary.Email);
+                if (usrEmail != null && !string.IsNullOrEmpty(summary.Email) && usrEmail.Id != summary.Id)
+                {
+                    AddNotification("Usuários", string.Format("Email '{0}' está sendo utilizado por outro usuário", summary.Email));
                 }
 
                 if (IsInvalid())
